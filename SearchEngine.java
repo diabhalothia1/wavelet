ccb387f0e371bf4a0fc5230f0cccfe80cd6f1edf
diabhalothia1 <100493743+diabhalothia1@users.noreply.github.com>
2022-10-14 20:06:11 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,3 +1,36 @@
-public class SearchEngine {
+import java.io.IOException;
+import java.net.URI;
+import java.util.*; 
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+   
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add")) 
+        {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                List<String> s = new ArrayList<>();
+                s.add(parameters[1]);
+                String[] k=new String[s.size()];
+                k=s.toArray(k);
+                return String.format("The new List is",k[0]);
+            }
+        
+        }
+        return "404 Not Found!";
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
     
+    }
 }
