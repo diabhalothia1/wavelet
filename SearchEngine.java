56ecd652ce1d002a125c66be995cf2f1c2e52e1f
diabhalothia1 <100493743+diabhalothia1@users.noreply.github.com>
2022-10-14 20:36:31 -0700

    N

--- SearchEngine.java
+++ SearchEngine.java
@@ -5,18 +5,33 @@ import java.util.*;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-   
+    List<String> s = new ArrayList<>();
 
     public String handleRequest(URI url) {
         if (url.getPath().contains("/add")) 
-        {
+        { 
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
-                List<String> s = new ArrayList<>();
                 s.add(parameters[1]);
-                String[] k=new String[s.size()];
-                k=s.toArray(k);
-                return String.format("The new List is",k[0]);
+                for(String nw:s){
+                    return s.toString();
+                }
+            }
+        
+        }
+        else if (url.getPath().contains("/search")) 
+        { 
+            String[] parameters1 = url.getQuery().split("=");
+            if (parameters1[0].equals("s")) {
+                List<String> s1 = new ArrayList<>();
+                
+                for(String nw:s){
+                    if(nw.contains(parameters1[1])){
+                        s1.add(nw);
+                    }
+                     
+                }
+                return s1.toString();
             }
         
         }
@@ -26,10 +41,13 @@ class Handler implements URLHandler {
 
 class SearchEngine {
     public static void main(String[] args) throws IOException {
+    
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
 
     
     }
